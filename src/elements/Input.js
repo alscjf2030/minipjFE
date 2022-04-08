c342c41869e2bdd963621746b04531e4f08d7d26
Kopite93 <1000nulee@naver.com>
2022-04-09 02:02:17 +0900

    220409

--- src/elements/Input.js
+++ src/elements/Input.js
@@ -3,11 +3,14 @@ import styled from "styled-components";
 
 const Input = (props) => {
   const { type, onChange, padding, margin, width } = props;
-  const styles = { padding, margin, width };
+  const styles = { padding: padding, margin: margin, width: width };
   return <TextInput type={type} onChange={onChange} {...styles}></TextInput>;
 };
 
 const TextInput = styled.input`
+  outline: none;
+  border: none;
+  border-bottom: 1px solid black;
   width: ${(props) => props.width};
   ${(props) => (props.padding ? `padding: ${props.padding};` : "")}
   ${(props) => (props.margin ? `margin: ${props.margin};` : "")}
