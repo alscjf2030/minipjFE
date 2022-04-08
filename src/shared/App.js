96aebe3960790a20ae3be820c70b81ea1ce8c5ec
Kopite93 <1000nulee@naver.com>
2022-04-08 17:42:17 +0900

    220408

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import AddComments from "../component/AddComments";
+import AddContents from "./../component/AddContents";
+import AddImage from "../component/AddImage";
 
 function App() {
   return (
     <div className="App">
+      <AddContents></AddContents>
       <AddComments></AddComments>
+      <AddImage></AddImage>
     </div>
   );
 }
