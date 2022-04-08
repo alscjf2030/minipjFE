afd3b86b28a5ee40e9fac0bbd656821fcfaa6779
alscjf2030 <101077108+alscjf2030@users.noreply.github.com>
2022-04-08 18:46:34 +0900

    'first'

---
+++ src/component/DetailComments.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+import Button from "../elements/Button";
+import Grid from "../elements/Grid";
+
+const DetailConmments = (props) => {
+
+    return(
+        <Grid>
+
+        </Grid>
+    )
+}
+
+export default DetailConmments
