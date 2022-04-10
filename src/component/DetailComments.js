1693b14c98e5d757ad85edcb05863f551202b13c
Kopite93 <1000nulee@naver.com>
2022-04-10 15:01:34 +0900

    220410

--- src/component/DetailComments.js
+++ src/component/DetailComments.js
@@ -1,16 +1,27 @@
-import React, { useEffect } from "react";
-
+import React, { useState, useEffect } from "react";
+import Button from "../elements/Button";
 import Grid from "../elements/Grid";
-import Image from "../elements/Image";
-import Text from "../elements/Text";
+import PostInput from "../elements/PostInput";
+import Modal from "react-modal";
 import { useDispatch, useSelector } from "react-redux";
 import { actionCreators as commentActions } from "../redux/modules/comment";
-import Button from "../elements/Button";
 
 const DetailComments = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
   const dispatch = useDispatch();
-  const comment = useSelector((state) => state.comment.comment);
-  const token = sessionStorage;
+  // const comment = useSelector((state) => state.comment.comment);
+  const comment = [
+    {
+      userInfo: {
+        nickname: "kop",
+      },
+      comment: "와아아아ㅏ아",
+    },
+  ];
+  // const token = sessionStorage;
+  const modalIsOpen = () => {
+    setIsOpen(true);
+  };
 
   useEffect(() => {
     dispatch(
@@ -33,18 +44,54 @@ const DetailComments = (props) => {
             <span>{cur.userInfo.nickname}</span>
             <p>{cur.comment}</p>
           </Grid>
-          <Button
-            onClick={() => {
-              dispatch(
-                commentActions.deleteCommentSP(
-                  cur,
-                  sessionStorage.getItem("jwt_token")
-                )
-              );
+          <Grid is_flex>
+            <Button
+              onClick={() => {
+                dispatch(
+                  commentActions.deleteCommentSP(
+                    cur,
+                    sessionStorage.getItem("jwt_token")
+                  )
+                );
+              }}
+            >
+              삭제
+            </Button>
+            <Button onClick={modalIsOpen}>수정</Button>
+          </Grid>
+          <Modal
+            isOpen={isOpen}
+            style={{
+              overlay: { margin: "auto" },
+              content: {
+                width: "820px",
+                height: "400px",
+                margin: "auto",
+                border: "2px solid black",
+              },
             }}
           >
-            삭제
-          </Button>
+            <Grid
+              // border={"1px solid black"}
+              width={"660px"}
+              height={"150px"}
+              margin={"60px auto -20px auto"}
+            >
+              <PostInput width={"600px"} height={"80px"} margin={"auto"}>
+                {cur.userInfo.nickname}
+              </PostInput>
+            </Grid>
+            <Grid is_flex width={"300px"} margin={"0 auto"}>
+              <Button>수정</Button>
+              <Button
+                onClick={() => {
+                  setIsOpen(false);
+                }}
+              >
+                취소
+              </Button>
+            </Grid>
+          </Modal>
         </Grid>
       ))}
     </>
