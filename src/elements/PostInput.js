75daae6093d720a360b115762d10df647439d00e
Kopite93 <1000nulee@naver.com>
2022-04-11 09:37:41 +0900

    220411

--- src/elements/PostInput.js
+++ src/elements/PostInput.js
@@ -8,9 +8,13 @@ const PostInput = (props) => {
     margin: props.margin,
   };
   return (
-    <Grid padding="0 30px">
+    <Grid padding="0 30px" margin={props.margin}>
       <p>{props.children}</p>
-      <TextInput onChange={props.onChange} {...styles} />
+      <TextInput
+        onChange={props.onChange}
+        defaultValue={props.defaultValue}
+        {...styles}
+      />
     </Grid>
   );
 };
@@ -22,7 +26,7 @@ PostInput.defaultProps = {
 };
 
 const TextInput = styled.textarea`
-  // ${(props) => (props.margin ? `margin: ${props.margin};` : "")}
+  ${(props) => (props.margin ? `margin: ${props.margin};` : "")}
   font-size: 1.1em;
   padding: 10px;
   border-radius: 10px;
